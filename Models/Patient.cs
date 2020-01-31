cf8409f572091119a98ec1b2765fa877212cb1b1
sami akriche <sami.akriche@esprit.tn>
2020-01-31 16:06:34 +0100

    first commit

---
+++ Models/Patient.cs
@@ -0,0 +1,11 @@
+using System;
+public class Patient {
+public int Id { get; set; }
+public string Name{ get; set; }
+public string AfterName { get; set; }
+public string Phone { get; set; }
+public string Address { get; set; }
+public DateTime BirthDate{ get; set; }
+public bool isActive{ get; set; }
+
+}
